99cddf88bf2be67edaaab5df87c93c978a432f50
Alberto <eks.alberto@gmail.com>
2019-02-20 20:48:48 +1100

    created components contact, experience and added react-router

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -2,12 +2,14 @@
 import React from "react";
 // Importando os components necessários da lib react-materialize
 import { Navbar, NavItem, Row} from 'react-materialize';
+// Importando o Component NavLink da nossa lib de rotas
+import { NavLink } from 'react-router-dom'
 
 const Header = () => (
   <Row>
     <Navbar className="grey darken-2">
-      <NavItem href='/'>Home</NavItem>
-      <NavItem href='/contact'>Contact</NavItem>
+      <li><NavLink to="/">Home</NavLink></li>
+      <li><NavLink to="contact">Contact</NavLink></li>
     </Navbar>
   </Row>
 );
